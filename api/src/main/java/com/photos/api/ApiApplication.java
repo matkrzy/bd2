7b5a75d8e8031dbac0fdb55b361e65205b7146b1
Michał Królewski <michal96krol@gmail.com>
2018-04-07 14:09:52 +0200

    Initial commit v2

--- api/src/main/java/com/photos/api/ApiApplication.java
+++ api/src/main/java/com/photos/api/ApiApplication.java
@@ -1,12 +1,12 @@
-package com.photos.system;
+package com.photos.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class ApiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        SpringApplication.run(ApiApplication.class, args);
     }
 }
